767ef68c56c81a79e9af0654bcc275f23770db38
Edouard <edouard@voltajazz.com>
2022-06-21 10:39:45 +0200

    Commit début s&ance 4

--- themes/ESGI/footer.php
+++ themes/ESGI/footer.php
@@ -1,9 +1,15 @@
-		<footer id="site-footer">
-			<div class="container">
-				<small>WP-ESGI theme by YoThemes</small>
-				<?php get_search_form() ?>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6 offset-md-3">
+					<footer id="site-footer">
+						<div class="container">
+							<small>WP-ESGI theme by YoThemes</small>
+							<?php get_search_form() ?>
+						</div>
+					</footer>
+				</div>
 			</div>
-		</footer>
+		</div>
 		<?php wp_footer(); ?>
 	</body>
 </html>
--- themes/ESGI/single.php
+++ themes/ESGI/single.php
@@ -1,37 +1,48 @@
 <?php get_header() ?>
-<main id="site-content">
+<main id="site-content" >
 	<div class="container">
-		<article>
-			<header>
-				<h1><?php the_title() ?></h1>
-				<div class="post-meta">
-					<div class="post-author">
-						<?php
-						$author_id = $post->post_author;
-						$author_name = get_the_author_meta( 'nickname', $author_id );
-						?>
-						<img src="<?= get_avatar_url($author_id) ?>" alt="<?= $author_name ?>">
-						<?= $author_name ?>
-					</div>
-					<time>
-						<?= date_i18n( 'j F Y', $post->post_date); ?>
-					</time>
-				</div>
-			</header>
-			
-			<?= get_the_post_thumbnail() ?>
-			<?php the_content() ?>
+		<div class="row">
+			<div class="col-md-6 offset-md-3">
+				<article class="single-post">
+					<header>
+						<h1 class="post-title"><?php the_title() ?></h1>
+						<div class="post-meta">
+							<div class="post-author">
+								<?php
+								$author_id = $post->post_author;
+								$author_name = get_the_author_meta( 'nickname', $author_id );
+								?>
+								<img src="<?= get_avatar_url($author_id) ?>" alt="<?= $author_name ?>">
+								<?= $author_name ?>
+							</div>
 
-			<footer>
-				<h2>Partager :</h2>
-				<ul>
-					<li><a href="#"><?= getIcon('twitter'); ?></a></li>
-					<li><a href="#"><?= getIcon('facebook'); ?></a></li>
-					<li><a href="#"><?= getIcon('google'); ?></a></li>
-					<li><a href="#"><?= getIcon('linkedin'); ?></a></li>
-				</ul>
-			</footer>
-		</article>
+							<time>
+								<?= date_i18n( 'j F Y', $post->post_date); ?>
+							</time>
+						</div>
+					</header>
+					<div class="post-image">
+						<?= get_the_post_thumbnail() ?>
+					</div>
+					<div class="post-content">
+						<?php the_content() ?>
+					</div>
+					
+
+					<footer>
+						<div class="social-block">
+							<h1>Partager :</h1>
+							<ul>
+								<li><a href="#"><?= getIcon('twitter'); ?></a></li>
+								<li><a href="#"><?= getIcon('facebook'); ?></a></li>
+								<li><a href="#"><?= getIcon('google'); ?></a></li>
+								<li><a href="#"><?= getIcon('linkedin'); ?></a></li>
+							</ul>
+						</div>
+					</footer>
+				</article>
+			</div>
+		</div>
 	</div>
 </main>
 <?php get_footer() ?>
